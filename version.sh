f05e205a9ce5fe6a005e304ab7acef2350aeb53b
Sanjeev Kumar Rao B <sanjeevkumarrao@gmail.com>
2025-07-01 13:21:57 +1000

    Fix critical bug in version.sh: Properly quote command arguments
    - Fixed issue where PREFIX, SUFFIX, MODULE, and BRANCH arguments were not properly quoted
    - This was causing argument parsing errors like 'argument -s/--suffix: expected one argument'
    - Changed from: main.py -s -beta to: main.py -s "-beta"
    - Added eval to properly execute the quoted command
    - This fixes prefix, suffix, and module versioning functionality

--- version.sh
+++ version.sh
@@ -13,19 +13,19 @@ elif [ -z "${GIT_REPO_PATH:-}" ]; then
 elif [ ! -z "${PREFIX:-}" ]; then
     echo "Prefix present in environment variable"
     VERSION_FILE="${GIT_REPO_PATH}/${VERSION_FILE}"
-    command="${command} -p ${PREFIX} -f ${VERSION_FILE} -r ${GIT_REPO_PATH}"
+    command="${command} -p \"${PREFIX}\" -f \"${VERSION_FILE}\" -r \"${GIT_REPO_PATH}\""
 elif [ ! -z "${SUFFIX:-}" ]; then
     echo "Suffix present in environment variable"
     VERSION_FILE="${GIT_REPO_PATH}/${VERSION_FILE}"
-    command="${command} -s ${SUFFIX} -f ${VERSION_FILE} -r ${GIT_REPO_PATH}"
+    command="${command} -s \"${SUFFIX}\" -f \"${VERSION_FILE}\" -r \"${GIT_REPO_PATH}\""
 elif [ ! -z "${MODULE:-}" ]; then
     echo "Module present in environment variable"
     VERSION_FILE="${GIT_REPO_PATH}/${VERSION_FILE}"
-    command="${command} -m ${MODULE} -f ${VERSION_FILE} -r ${GIT_REPO_PATH}"
+    command="${command} -m \"${MODULE}\" -f \"${VERSION_FILE}\" -r \"${GIT_REPO_PATH}\""
 else
     echo "Getting a simple semver"
     VERSION_FILE="${GIT_REPO_PATH}/${VERSION_FILE}"
-    command="${command} -f ${VERSION_FILE} -r ${GIT_REPO_PATH}"
+    command="${command} -f \"${VERSION_FILE}\" -r \"${GIT_REPO_PATH}\""
 fi
 
 # FIXED: Only add -i flag when IS_SNAPSHOT is explicitly "true"
@@ -36,7 +36,7 @@ fi
 
 if [ ! -z "${BRANCH:-}" ]; then
   echo "Branch present in environment variable"
-  command="${command} -b ${BRANCH}"
+  command="${command} -b \"${BRANCH}\""
 fi
 
 echo $command
@@ -51,4 +51,4 @@ if [ -f "$GITHUB_ACTION_PATH/requirements.txt" ]; then
 fi
 
 # Run the improved Python script with the variables
-python3 $GITHUB_ACTION_PATH/$command
+eval "python3 $GITHUB_ACTION_PATH/$command"
